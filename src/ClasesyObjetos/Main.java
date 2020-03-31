f35d3082696abafc0613552f832c046133394c98
Sebastian Vargas <sebastian.vargas@utp.edu.co>
2020-03-30 19:43:38 -0500

    Metodo Constructor como Metodo (sin this)

---
+++ src/ClasesyObjetos/Main.java
@@ -0,0 +1,13 @@
+
+package ClasesyObjetos;
+
+public class Main {
+    
+    public static void main(String[] args){
+        
+        //instanciamos 
+        Persona p1 = new Persona("Sebastian",21);
+        p1.mostrarDatos();
+    }
+    
+}
---
+++ src/ClasesyObjetos/Persona.java
@@ -0,0 +1,21 @@
+
+package ClasesyObjetos;
+
+public class Persona {
+    //atributos
+    String nombre;
+    int edad;
+    
+    //metodo constructor
+    public Persona(String _nombre, int _edad){
+        nombre = _nombre; 
+        edad = _edad;      
+    }
+    
+    public void mostrarDatos(){
+        System.out.println("El nombre es: "+nombre);
+        System.out.println("La edad es: "+edad);
+    }
+    
+    
+}
