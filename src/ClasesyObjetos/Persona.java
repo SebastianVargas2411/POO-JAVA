d044e159ebf21a00bef0885fe541e676b58a5b2c
Sebastian Vargas <sebastian.vargas@utp.edu.co>
2020-03-30 20:09:14 -0500

    Metodo Constructor SOBRECARGA DE METODOS

--- src/ClasesyObjetos/Persona.java
+++ src/ClasesyObjetos/Persona.java
@@ -5,17 +5,30 @@ public class Persona {
     //atributos
     String nombre;
     int edad;
-    
-    //metodo constructor
-    public Persona(String nombre, int edad){
-        this.nombre = nombre; //this para decir cual es atributo
-        this.edad = edad; // el que no tiene this es el parametro osea el valor a asignarle al atributo     
+    String identificacion;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Persona(String identificacion) {
+        this.identificacion = identificacion;
+    }
+    //En la sobrecarga de metodos java diferencia el mismo metodo por el tipo de dato
+    public void correr(){
+        System.out.println("soy"+nombre+"tengo"+edad+"años y corro maraton");    
     }
     
-    public void mostrarDatos(){
-        System.out.println("El nombre es: "+nombre);
-        System.out.println("La edad es: "+edad);
+    public void correr(int km){
+        System.out.println("He corrido"+km+"kilometros");
+    
     }
+    
+            
+         
+    
+    
     
     
 }
